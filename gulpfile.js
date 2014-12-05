67d6591353e0d7386a055891bb745d1ea5d6fbe1
Elger van Boxtel <elgervb@gmail.com>
2014-12-05 08:36:25 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    express = require('express'),
+    del = require('del'),
+    browserSync = require('browser-sync');
+
+gulp.task('styles', function() {
+  return gulp.src('src/styles/main.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/js/**/*.js')
+    // .pipe(jshint('.jshintrc'))
+    // .pipe(jshint.reporter('default'))
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/assets/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('clean', function(cb) {
+    del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img', 'dist/assets/fonts'], cb)
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images', 'copy-fonts');
+});
+
+gulp.task('copy-fonts', function() {
+    return gulp.src(['src/fonts/*'])
+        .pipe(gulp.dest('dist/assets/fonts'));
+});
+
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/js/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/img/**/*', ['images']);
+
+});
+
+gulp.task('live-reload', ['watch'], function() {
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/* & index.html, reload on change
+  gulp.watch(['dist/**', 'index.html']).on('change', livereload.changed);
+  
+
+});
+
+// browser-sync task for starting the server.
+gulp.task('browser-sync', ['watch'], function() {
+
+  // Watch any files in dist/* & index.html, reload on change
+  gulp.watch(['dist/**', 'index.html']).on('change', function(){browserSync.reload({});notify({ message: 'Reload browser' });});
+
+  return browserSync({
+      server: {
+          baseDir: "./"
+      },
+      ghostMode: {
+        clicks: true,
+        location: true,
+        forms: true,
+        scroll: true
+      },
+      open: "external",
+      injectChanges: true, // inject CSS changes (false force a reload) 
+      browser: ["google chrome"],
+      scrollProportionally: true, // Sync viewports to TOP position
+      scrollThrottle: 50,
+    });
+});
+
+gulp.task('express', function(){
+  var app = express();
+  app.use(express.static(__dirname));
+  app.listen(4000); 
+});
+
+gulp.task('express-lr', ['express', 'live-reload'], function(){});
