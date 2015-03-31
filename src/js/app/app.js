92dee6d6a33db031d547e44658370bffe3f11d83
Elger van Boxtel <elgervb@gmail.com>
2015-03-31 09:01:09 +0200

    lightweight version of the skeleton

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -4,10 +4,6 @@ var app = angular.module('default', ['ngRoute'])
 .config(function($routeProvider, $locationProvider) {
  
   $routeProvider
-	.when('/about', {
-	  controller: 'AboutController',
-	  templateUrl: '/assets/js/app/modules/about/about.html'
-	})
   .otherwise({
     controller: 'MainController',
     templateUrl: '/assets/js/app/modules/main/main.html'
