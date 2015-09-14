30dce006ed93254736d514a18f87632b3b75852f
Elger van Boxtel <elgervb@gmail.com>
2015-09-14 16:44:08 +0200

    run in single browser window

--- protractor.config.js
+++ protractor.config.js
@@ -1,11 +1,11 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  // capabilities: { // running agains a single browser
-  //   browserName: 'phantomjs' // 'chrome', 'firefox'
-  // }
-  multiCapabilities: [{ // running agains multiple browsers
-    browserName: 'firefox'
-  }, {
-    browserName: 'chrome'
-  }]
+  capabilities: { // running agains a single browser
+    browserName: 'chrome' // 'chrome', 'firefox'
+  }
+  // multiCapabilities: [{ // running agains multiple browsers
+  //   browserName: 'firefox'
+  // }, {
+  //   browserName: 'chrome'
+  // }]
 };
