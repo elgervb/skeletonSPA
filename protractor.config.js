49582f50cb8bff664ea8359b768d80a6fef851fa
Elger van Boxtel <elgervb@gmail.com>
2015-09-17 12:57:59 +0200

    Run e2e tests in Travis

--- protractor.config.js
+++ protractor.config.js
@@ -1,30 +1,34 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  directConnect: true,
+  /**
+   * Direct connect with a real browser, not supported for PhantomJS
+   */
+  // directConnect: true,
+  
   /**
    * Run in PhantomJS
    */
-  // capabilities: {
-  //   'browserName': 'phantomjs',
-  //   /* 
-  //   * Can be used to specify the phantomjs binary path.
-  //   * This can generally be ommitted if you installed phantomjs globally.
-  //   */
-  //   'phantomjs.binary.path': require('phantomjs').path,
+  capabilities: {
+    'browserName': 'phantomjs',
+    /* 
+    * Can be used to specify the phantomjs binary path.
+    * This can generally be ommitted if you installed phantomjs globally.
+    */
+    'phantomjs.binary.path': require('phantomjs').path,
   
-  //   /*
-  //   * Command line args to pass to ghostdriver, phantomjs's browser driver.
-  //   * See https://github.com/detro/ghostdriver#faq
-  //   */
-  //   'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
-  // },
+    /*
+    * Command line args to pass to ghostdriver, phantomjs's browser driver.
+    * See https://github.com/detro/ghostdriver#faq
+    */
+    //'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+  },
   
   /**
    * Run in real browser: chrome, firefox
    */
-  capabilities: { // running agains a single browser
-    browserName: 'chrome', // 'chrome', 'firefox', 'internet explorer'
-  },
+  // capabilities: { // running agains a single browser
+  //   browserName: 'chrome', // 'chrome', 'firefox', 'internet explorer'
+  // },
   
   /**
     * Run in multiple browsers
@@ -37,6 +41,6 @@ exports.config = {
   framework: 'jasmine',
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 10000
+    defaultTimeoutInterval: 30000
   }
 };
