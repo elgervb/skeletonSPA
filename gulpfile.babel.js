b815acd824ca11bad52deac0429d898d84beb8d4
Elger van Boxtel <elger.vanboxtel@planonsoftware.com>
2016-02-09 21:33:59 +0100

    replace gulp-minify-css by gulp-cssnano
    fixed #64

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -330,7 +330,7 @@ gulp.task('start', ['browser-sync']);
 gulp.task('styles', ['styles-vendor', 'styles-copy-sass'], () => {
   let autoprefixer = require('gulp-autoprefixer');
   let cmq = require('gulp-group-css-media-queries');
-  let minifycss = require('gulp-minify-css');
+  let cssnano = require('gulp-cssnano');
   let sass = require('gulp-sass');
   let sourcemaps = require('gulp-sourcemaps');
   
@@ -346,8 +346,8 @@ gulp.task('styles', ['styles-vendor', 'styles-copy-sass'], () => {
   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
   .pipe(gulpif(argv.dev, sourcemaps.write('./', {sourceRoot: '/css/sass'})))
   .pipe(gulpif(!argv.dev, cmq())) // only combine media queries when not generating source maps, as this breaks them
-  .pipe(gulpif(!argv.dev, minifycss()))
-  .pipe(size({showFiles: true}))
+  .pipe(gulpif(!argv.dev, cssnano()))
+  .pipe(size({showFiles: true}))  
   
   .pipe(gulp.dest(`${settings.dist}css`));
   
