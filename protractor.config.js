b5cb3f54e4e90fc661ca11def3e01998059c152f
Elger van Boxtel <elger.vanboxtel@planonsoftware.com>
2016-03-01 13:33:34 +0100

    run e2e tests with PhantomJS

--- protractor.config.js
+++ protractor.config.js
@@ -7,32 +7,32 @@ exports.config = {
   /**
    * Run in PhantomJS
    */
-  // capabilities: {
-  //   'browserName': 'phantomjs',
-  //   /* 
-  //   * Can be used to specify the phantomjs binary path.
-  //   * This can generally be ommitted if you installed phantomjs globally.
-  //   */
-  //   'phantomjs.binary.path': require('phantomjs').path,
+  capabilities: {
+    'browserName': 'phantomjs',
+    /* 
+    * Can be used to specify the phantomjs binary path.
+    * This can generally be ommitted if you installed phantomjs globally.
+    */
+    'phantomjs.binary.path': require('phantomjs').path,
   
-  //   /*
-  //   * Command line args to pass to ghostdriver, phantomjs's browser driver.
-  //   * See https://github.com/detro/ghostdriver#faq
-  //   */
-  //   'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
-  // },
+    /*
+    * Command line args to pass to ghostdriver, phantomjs's browser driver.
+    * See https://github.com/detro/ghostdriver#faq
+    */
+    'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+  },
   
   
   /**
    * Run in real browser: chrome, firefox
    */
-  capabilities: { // running agains a single browser
-    browserName: 'chrome', // 'chrome', 'firefox', 'internet explorer'
-  },
+  // capabilities: { // running agains a single browser
+  //   browserName: 'chrome', // 'chrome', 'firefox', 'internet explorer'
+  // },
   /**
    * Direct connect with a real browser, not supported for PhantomJS
    */
-  directConnect: true,
+  // directConnect: true,
   
   /**
     * Run in multiple browsers
