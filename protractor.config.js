35492e563e740b13300f0d9ea4da0bce9831c1a5
Elger van Boxtel <elger.vanboxtel@planonsoftware.com>
2016-03-03 20:37:08 +0100

    protractor use es2015 presets

--- protractor.config.js
+++ protractor.config.js
@@ -3,7 +3,7 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   onPrepare: function () {
-      require("babel-register");
+      require("babel-register")({presets: ["es2015"]})
   },
   
   /**
