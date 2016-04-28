8431254498e2864ca6f2d4436b0a1ae549ba21e8
Elger van Boxtel <elgervb@gmail.com>
2016-04-28 23:37:20 +0200

    merge controller from service

--- client/app/components/game/game.controller.js
+++ client/app/components/game/game.controller.js
@@ -1,26 +1,45 @@
 class GameController {
   constructor($interval, gameService) {
     'ngInject';
+    this.name = 'game';
+    
     this.service = gameService;
     this.$interval = $interval;
-    this.progress = 0;
-    this.name = 'game';
+    
+    this.progression = 0;
+    this.inProgress = false;
+    this.color = {};
+    this.startTime = -1;
   }
   
-  inProgress() {
-    return this.service.inProgress;
+  progress() {
+    return ((new Date().getTime() - this.startTime) / 1000).toFixed(1);
+  }
+  
+  /**
+   * Generates a new random color
+   * @returns {number} a six figure hex color code
+   */
+  randomColor() {
+    return ('000000' + (Math.random()*0xFFFFFF<<0).toString(16)).slice(-6);
   }
   
   start() {
-    this.service.start();
-    this.color = this.service.color;
+    this.color.guess = this.randomColor();
+    this.color.init = this.randomColor();
+    
+    this.inProgress = true;
+    this.startTime = new Date().getTime();
     
     let interval = 100;
-    this.$interval(() => {this.progress = this.service.progress()}, interval);
-  }
+    this.$interval(() => {this.progression = this.progress()}, interval);
+  } 
   
   reset() {
-    this.service.reset();
+    delete this.color.guess;
+    delete this.color.init;
+    this.inProgress = false;
+    
     if (this.timer){
       this.$interval.cancel(this.timer);
     }
--- client/app/components/game/game.service.js
+++ client/app/components/game/game.service.js
@@ -1,34 +1,6 @@
 class GameService {
   constructor() {
-    this.inProgress = false;
-    this.color = {};
-    this.startTime = -1;
-  }
-  
-  start() {
-    this.color.guess = this.randomColor();
-    this.color.init = this.randomColor();
     
-    this.inProgress = true;
-    this.startTime = new Date().getTime();
-  }
-  
-  progress() {
-    return ((new Date().getTime() - this.startTime) / 1000).toFixed(1);
-  }
-  
-  reset() {
-    delete this.color.guess;
-    delete this.color.init;
-    this.inProgress = false;
-  }
-  
-  /**
-   * Generates a new random color
-   * @returns {number} a six figure hex color code
-   */
-  randomColor() {
-    return ('000000' + (Math.random()*0xFFFFFF<<0).toString(16)).slice(-6);
   }
   
   static factory() {
