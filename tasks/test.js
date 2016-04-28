4e24b43d1f72059a06005ec79733f04097795db8
Elger van Boxtel <elgervb@gmail.com>
2016-04-28 09:26:13 +0200

    gulp testing tasks

--- tasks/test.js
+++ tasks/test.js
@@ -1,10 +1,10 @@
+import karma from 'karma';
 /**
  * Run tests and keep watching changes for files
  */
-module.exports = (gulp, plugins, settings) => {
+module.exports = (gulp, settings) => {
   return function (done) {
-    let Server = plugins.karma.Server;
-    new Server({
+    new karma.Server({
       configFile: `${__dirname}/../karma.conf.js`,
       singleRun: true
     }, () => {done();}).start();
