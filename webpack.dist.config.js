25b33c8a79b3b53b6173d16719de87e7fb5ae375
Elger van Boxtel <elgervb@gmail.com>
2016-05-23 20:29:28 +0200

    no warnings

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -17,7 +17,10 @@ config.plugins = config.plugins.concat([
       // For example if your vendor dependency doesn't use modules
       // and relies on global variables. Most of angular modules relies on
       // angular global variable, so we should keep it unchanged
-      // except: ['$super', '$', 'exports', 'require', 'angular']
+      // except: ['$super', '$', 'exports', 'require', 'angular', 'moment'],
+      compress: {
+        warnings: false
+      }
     }
   })
 ]);
